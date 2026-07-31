408acd3568b6ae5d3e39ea29f9ad006315ef2564
Fesk123 <fesk08001@utb.vaxjo.se>
2026-07-31 14:04:37 +0200

    Clarify module termination with endmodule comment
    Added endmodule comment to clarify module termination.

--- AND_gate.v
+++ AND_gate.v
@@ -13,12 +13,4 @@ module AND_gate
     assign and_temp = input_1 & input_2; // & is the AND operator
     assign output_1 = and_temp; // Assigns the value of and_temp to the output
 
-    initial begin
-        $dumpfile("gate.vcd"); // Names the output file for the waveform
-        $dumpvars(0, AND_gate);
-
-        #50
-        $finish;
-    end
-
 endmodule // Ends the module definition
