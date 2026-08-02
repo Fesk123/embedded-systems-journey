f9da82ae1cc23a94b40e5d15e00abd0fae8db984
Fesk123 <fesk08001@utb.vaxjo.se>
2026-08-02 20:00:31 +0200

    Uploaded decoder code

---
+++ Decoder.v
@@ -0,0 +1,36 @@
+module decoder(
+    input_1,
+    input_2,
+    output_1,
+    output_2,
+    output_3,
+    output_4);
+
+    input input_1, input_2;
+    output output_1, output_2, output_3, output_4;
+
+    // Defines the inputs and outputs of the module as before
+
+    wire not_input_1, not_input_2;
+
+    // These wires are used to hold the inverted values of the inputs.
+
+    not (
+        not_input_1,
+        input_1
+    );
+    not (
+        not_input_2,
+        input_2
+    );
+
+    // The not gates invert the values of the inputs. If input_1 is 0, not_input_1 will be 1, and vice versa.
+
+    and (output_1, not_input_1, not_input_2);
+    and (output_2, not_input_1, input_2);
+    and (output_3, input_1, not_input_2);
+    and (output_4, input_1, input_2);
+
+    // The and gates combine the inverted and non-inveted inputs to produce the outputs. Each output corresponds to a unique combination of the inputs.
+
+endmodule
